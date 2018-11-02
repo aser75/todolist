1681c70efc22c4a9ac93c07e57a5d2d3e9fcfe41
aser75 <max.puppetto@yahoo.com>
2018-11-02 12:39:05 +0100

    cache firebase token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBSux8ygdtJ4S50UviWZdeRMhqEiXzZtuE",
-    authDomain: "todolist-7008c.firebaseapp.com",
-    databaseURL: "https://todolist-7008c.firebaseio.com",
-    projectId: "todolist-7008c",
-    storageBucket: "todolist-7008c.appspot.com",
-    messagingSenderId: "294924192583"
-  }
+
 };
 
 /*
